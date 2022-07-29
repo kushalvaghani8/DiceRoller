dc938d9d1be1d8c9702de2c2486ef88974842410
kushalvaghani <A00247163@mycambrian.ca>
2022-07-29 12:33:38 -0400

    Initial Commit & Test
    Initial Commit along with first test, asserting that die class can be initiated

---
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DiceRoller.Models
+{
+    public class Die
+    {
+        /*
+        public string Name { get; set; }
+
+        public int NumSides { get; set; }
+
+        public int CurrentSide { get; set; }
+
+        public Die()
+        {
+            Name = "d6";
+            NumSides = 6;
+            Roll();
+        }
+
+        public Die(int numSides)
+        {
+            Name = "d" + numSides;
+            NumSides = numSides;
+            Roll();
+        }
+
+        public void Roll()
+        {
+            Random r = new Random();
+            CurrentSide = r.Next(NumSides) + 1;
+        }*/
+    }
+}
+
---
+++ DieUnitTests/DieUnitTests.cs
@@ -0,0 +1,49 @@
+﻿using DiceRoller.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace DieUnitTests
+{
+    [TestClass]
+    public class DieUnitTests
+    {
+        private Die d = new Die();
+
+        [TestMethod]
+        public void DieClassExists()
+        {
+            d.Should().NotBeNull();
+        }
+        /*
+        [TestMethod]
+        public void DieHasAllDefaultValues()
+        {
+            d.Name.Should().Be("d6");
+            d.NumSides.Should().Be(6);
+            d.CurrentSide.Should().BeInRange(1, 6);
+        }
+        
+        
+        [TestMethod]
+        public void RollSetsSideCorrectly()
+        {
+            for (int i = 0; i <1000; i++)
+            {
+                d.Roll();
+                d.CurrentSide.Should().BeInRange(1, 6);
+            }
+            
+        }
+        
+        [TestMethod]
+        public void DieHasCustomSides()
+        {
+            Die di = new Die(8);
+            di.Name.Should().Be("d8");
+            di.NumSides.Should().Be(8);
+            di.CurrentSide.Should().BeInRange(1, 8);
+        }
+        */
+    }
+}
+
