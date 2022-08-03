cd73178cba50ee4927f4fcac6e94ebaaccdaa40e
kushalvaghani <A00247163@mycambrian.ca>
2022-08-03 12:57:01 -0400

    Testing if die has all the default values
    The test helps to check if all the default values are set properly and is accessible, the test checks die has a default name of d6, number of sides (max) is set to 6 and when the roll method is called via getcurrentside the number is between the range of 1 to 6. The test assists in identifying if we missed to give any value.

--- DiceRoller/DiceRoller/Models/Die.cs
+++ DiceRoller/DiceRoller/Models/Die.cs
@@ -3,32 +3,44 @@ namespace DiceRoller.Models
 {
     public class Die
     {
-        /*
-        public string Name { get; set; }
+        
+        private string Name { get; set; }
 
-        public int NumSides { get; set; }
+        private int NumSides { get; set; }
 
-        public int CurrentSide { get; set; }
+        private int CurrentSide { get; set; }
 
-        public Die()
+        public Die() // 0 arguments
         {
             Name = "d6";
             NumSides = 6;
             Roll();
         }
 
-        public Die(int numSides)
+        public Die(int numSides) // 1 arugment passing in number of sides
         {
             Name = "d" + numSides;
             NumSides = numSides;
             Roll();
         }
 
-        public void Roll()
+        public void SetSideUp(int newSideUp) //setting up the current side to newSide
+        {
+            if(newSideUp >=1 && newSideUp <= NumSides)
+            this.CurrentSide = newSideUp;
+        }
+
+        public void Roll() // roll method, generating random number 
         {
             Random r = new Random();
             CurrentSide = r.Next(NumSides) + 1;
-        }*/
+        }
+
+        public String GetName() { return Name; } //getter for Name, after changing public method to private 
+
+        public int GetNumSides() { return NumSides; } //getter for Sides, after changing public method to private
+
+        public int GetCurrentSide() { return CurrentSide; } //getter for CurrentSide, after changing public method to private
     }
 }
 
--- DieUnitTests/DieUnitTests.cs
+++ DieUnitTests/DieUnitTests.cs
@@ -10,40 +10,110 @@ namespace DieUnitTests
         private Die d = new Die();
 
         [TestMethod]
-        public void DieClassExists()
+        public void DieClassExists() //checking if the class exists (and is accessable)
         {
             d.Should().NotBeNull();
         }
-        /*
+
+
         [TestMethod]
-        public void DieHasAllDefaultValues()
+        public void DieHasAllDefaultValues() //testing that the die has all the predefined values and the side is in range from 1 to 6
         {
-            d.Name.Should().Be("d6");
-            d.NumSides.Should().Be(6);
-            d.CurrentSide.Should().BeInRange(1, 6);
+            d.GetName().Should().Be("d6"); //calling the getter and comparing it with "d6"
+            d.GetNumSides().Should().Be(6);//calling the number of sides getter to check if the value of sides is 6
+            d.GetCurrentSide().Should().BeInRange(1, 6); //calling the method get current side to check if the rolled dice range is between 1 to 6
         }
-        
-        
-        [TestMethod]
-        public void RollSetsSideCorrectly()
-        {
-            for (int i = 0; i <1000; i++)
-            {
-                d.Roll();
-                d.CurrentSide.Should().BeInRange(1, 6);
-            }
-            
-        }
-        
-        [TestMethod]
-        public void DieHasCustomSides()
-        {
-            Die di = new Die(8);
-            di.Name.Should().Be("d8");
-            di.NumSides.Should().Be(8);
-            di.CurrentSide.Should().BeInRange(1, 8);
-        }
-        */
+
     }
 }
+ /*       
+        //[TestMethod]
+        //public void DefaultRollSetsSideCorrectly()
+        //{
+        //    for (int i = 0; i <1000; i++)
+        //    {
+        //        d.Roll();
+        //        d.CurrentSide.Should().BeInRange(1, 6);
+        //    }
+            
+        //}
 
+      
+
+        [TestMethod]
+        [DataRow(3 , "d3")] //passing in the value and checking if die is able to set custom sides, and name
+        [DataRow(4, "d4")] //also the range when roll method is called that the range is between 1 to the set number of sides
+        [DataRow(8, "d8")]
+        [DataRow(10, "d10")]
+        [DataRow(12, "d12")]
+        [DataRow(20, "d20")]
+        public void DieHasCustomSides(int sides, string name)
+        {
+            Die di = new Die(sides);
+            di.GetName().Should().Be(name);
+            di.GetNumSides().Should().Be(sides);
+            di.GetCurrentSide().Should().BeInRange(1, sides);
+        }
+
+
+
+        [TestMethod]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(8)]
+        [DataRow(10)]
+        [DataRow(12)]
+        [DataRow(20)]
+        public void RollSetsSideCorrectlyForCustomSides(int sides)
+        {
+            Die d = new Die(sides);
+            for (int i = 0; i < 1000; i++)
+            {
+                d.Roll();
+                d.GetCurrentSide().Should().BeInRange(1, sides);
+            }
+
+        }
+
+
+        [TestMethod]
+        [DataRow(3, 2)]
+        [DataRow(5, 10)]
+        [DataRow(10, -12)]
+        public void SetSideUpSetsValidSide(int sides, int newSide)
+        {
+            Die d = new Die(sides);
+            d.SetSideUp(newSide);
+            d.GetCurrentSide().Should().BeInRange(1, sides);
+            if (newSide >= 1 && newSide <= sides)
+            {
+                d.GetCurrentSide().Should().Be(newSide);
+            }
+
+        }
+
+
+        [TestMethod]
+        public void GetDefultNameReturnsValue()
+        {
+            d.GetName().Should().BeOfType<string>();
+            d.GetName().Should().Be("d6");
+        }
+
+
+        [TestMethod]
+        public void GetNumSidesReturnsValue()
+        {
+           
+            d.GetNumSides().Should().Be(6);
+        }
+
+
+        [TestMethod]
+        public void GetCurrentSideReturnsValue()
+        {
+            d.GetNumSides().Should().BeInRange(1, 6);
+        }
+
+    }
+}*/
